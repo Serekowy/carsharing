24ad0542579c6e6a94474e2173ee155d5fd46313
jakisgazu <gracjan.zareba0@wp.pl>
2025-05-26 21:29:46 +0200

    test

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );git
+    );
   }
 }
 
@@ -104,7 +104,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
+            const Text('jajco:'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
